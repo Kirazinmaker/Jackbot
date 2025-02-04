451a143acab9b54e5124710a29bb713d34ba0d82
Amal Rockz <97275372+Kirazinmaker@users.noreply.github.com>
2025-02-04 15:31:14 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const global = {
+    owner: [5052038904], // ganti jadi id mu
+    botToken: "7681328841:AAHhLodOODzdC_blDVoEP0Moernx7VrsMt0", //isi make token bot mu
+}
+
+module.exports = global;
